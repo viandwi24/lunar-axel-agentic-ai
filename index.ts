b4e2000adf25e9cc38f89290b1b3e3877b18a36b
Alfian Dwi Nugraha <viandwi24@pm.me>
2026-01-01 23:45:26 +0700

    feat: initialize

---
+++ index.ts
@@ -0,0 +1,15 @@
+import YahooFinance from "yahoo-finance2";
+
+const yh = new YahooFinance({
+  suppressNotices: ['yahooSurvey'],
+})
+
+// const res = await yh.search('XAUT-USD', { quotesCount: 5 });
+// console.log(res.quotes);
+
+const res = await yh.chart('XAUT-USD', {
+  period1: '2025-10-01',
+  period2: '2025-12-31',
+  interval: '1d',
+});
+console.log(res.quotes.length);
